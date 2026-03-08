5c33478961de3a6d9540589e7436830fea1d37e0
Daniel King <damaki.gh@gmail.com>
2026-03-08 19:39:25 +0000

    tests: Add options to prevent deleting working files
    This adds the options --working-dir and --keep-build-files to keep
    any intermediate working files that were used during testing instead
    of deleting them. This is useful for debugging on-target test failures
    as the executable is not deleted.

--- tests/conftest.py
+++ tests/conftest.py
@@ -22,6 +22,18 @@ def pytest_addoption(parser):
         default=None,
         help="Name of the hardware target to use for executing tests",
     )
+    parser.addoption(
+        "--working-dir",
+        action="store",
+        type=str,
+        default=None,
+        help="Directory to store working files for building tests",
+    )
+    parser.addoption(
+        "--keep-build-files",
+        action="store_true",
+        help="Don't delete working files after the test is completed",
+    )
 
 @pytest.fixture
 def gdbserver_port(request):
@@ -34,3 +46,11 @@ def text_io_port(request):
 @pytest.fixture
 def target_board(request):
     return request.config.getoption("--target-board")
+
+@pytest.fixture
+def working_dir(request):
+    return request.config.getoption("--working-dir")
+
+@pytest.fixture
+def keep_build_files(request):
+    return request.config.getoption("--keep-build-files")
