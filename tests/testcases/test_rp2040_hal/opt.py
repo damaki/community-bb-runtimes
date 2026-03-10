f648a4317d4be2f25cdc059a4eafddd13ba46258
Daniel King <damaki.gh@gmail.com>
2026-03-10 20:35:18 +0000

    tests: Add a testcase that checks basic compatibility with rp2040_hal

---
+++ tests/testcases/test_rp2040_hal/opt.py
@@ -0,0 +1,19 @@
+import support.target_info
+import pytest
+import pathlib
+
+
+def check_test_conditions(target_info: support.target_info.TargetInfo):
+    if target_info.target != "rp2040":
+        pytest.skip(f"Test is not applicable for target '{target_info.target}'")
+
+
+def external_crates(target_info: support.target_info.TargetInfo):
+    return {"rp2040_hal": "=2.7.0", "pico_bsp": "=2.2.0"}
+
+
+def crate_config_values(target_info: support.target_info.TargetInfo):
+    if target_info.runtime_profile == "light":
+        return {"rp2040_hal.Use_Startup": False}
+    else:
+        return {"rp2040_hal.Interrupts": "bb_runtimes", "rp2040_hal.Use_Startup": False}
